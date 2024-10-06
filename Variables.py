17676bdaebd9ed6e2375232cf3f3e432388f22e3
LyubovVK <lkoltcova66@gmail.com>
2024-10-06 22:15:05 +0300

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,6 @@
+# Переменные
+count_dz = 12
+count_hour = 1.5
+name_course = 'Python'
+time_for_one_task = count_hour / count_dz
+print('Курс: ' + name_course, '. Всего задач: ' + str(count_dz), '. Затрачено часов: ' + str(count_hour), '. Среднее время выполнения: ' + str(time_for_one_task))
